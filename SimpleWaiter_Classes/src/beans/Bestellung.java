7a4605e5607971b0dfb48b942d8f39363e29fc78
Jürgen <linjua13@htl-kaindorf.ac.at>
2017-11-23 12:47:42 +0100

    SimpleWaiter Klassen

--- SimpleWaiter_Classes/src/beans/Bestellung.java
+++ SimpleWaiter_Classes/src/beans/Bestellung.java
@@ -1,7 +1,38 @@
 
 package beans;
 
+import java.time.LocalDateTime;
+import java.util.Map;
+
 public class Bestellung 
 {
+    //Attribute
+    private long bestellid;
+    private LocalDateTime bestellzeit;
+    private Map<Getraenk, Integer> getraenke;
+    private double gesamtSumme;
+
+    public Bestellung(LocalDateTime bestellzeit, Map<Getraenk, Integer> getraenke, double gesamtSumme) {
+        this.bestellzeit = bestellzeit;
+        this.getraenke = getraenke;
+        this.gesamtSumme = gesamtSumme;
+    }
+    
+    //getter
+    public long getBestellid() {
+        return bestellid;
+    }
+
+    public LocalDateTime getBestellzeit() {
+        return bestellzeit;
+    }
+
+    public Map<Getraenk, Integer> getGetraenke() {
+        return getraenke;
+    }
+
+    public double getGesamtSumme() {
+        return gesamtSumme;
+    }
     
 }
