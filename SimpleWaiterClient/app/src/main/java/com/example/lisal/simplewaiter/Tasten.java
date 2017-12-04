8ec5594f940a8d012ed043e4d1b190b36498e71c
Lilly <lizz.poeschl@gmx.at>
2017-12-04 09:43:20 +0100

    last changes/ also alles

--- SimpleWaiterClient/app/src/main/java/com/example/lisal/simplewaiter/Tasten.java
+++ SimpleWaiterClient/app/src/main/java/com/example/lisal/simplewaiter/Tasten.java
@@ -1,17 +1,13 @@
 package com.example.lisal.simplewaiter;
 
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 
-public class Anzeige extends AppCompatActivity {
-
+public class Tasten extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_anzeige);
-
-
+        setContentView(R.layout.activity_tasten);
     }
-
 }
